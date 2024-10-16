dc40ac71910683ba54473ffe2d33301859238bbc
Carlos Rios <145784223+CarlosSoft04@users.noreply.github.com>
2024-10-16 17:28:20 +0000

    Criando tabela

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,1 +1,9 @@
 -- Crie uma nova tabela no banco de dados
+CREATE TABLE pessoa(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nome VARCHAR(20),
+idade DATE,
+descricao TEXT,
+altura double,
+peso DOUBLE
+);
